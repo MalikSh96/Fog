573d9e40a8d078c893cb790dea43090f7c2768a5
Malik <malik96sharfo@hotmail.com>
2018-04-24 10:58:14 +0200

    creating an orders is possible

--- src/main/java/dbaccess/OrderMapper.java
+++ src/main/java/dbaccess/OrderMapper.java
@@ -5,10 +5,47 @@
  */
 package dbaccess;
 
+import functionlayer.Order;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author malik
  */
 public class OrderMapper {
     
+    public void createPreOrder(Order ord) //To test in main
+    {
+        int orderId = 0;
+        try 
+        {
+            Connection con = Connector.connection();
+            String SQL = "INSERT INTO orders (userID, length, width, height) VALUES (?, ?, ?, ?)";
+
+            PreparedStatement ps = con.prepareStatement( SQL, Statement.RETURN_GENERATED_KEYS ); 
+            ps.setInt(1, ord.getUserId()); //user id
+            ps.setInt(2, ord.getLength());
+            ps.setInt(3, ord.getWidth());
+            ps.setInt(4, ord.getHeight());
+
+            System.out.println("Check sql order " + SQL);
+
+            int check = ps.executeUpdate();
+            if(check == 1)
+            {
+                ResultSet ids = ps.getGeneratedKeys();
+                ids.next();
+                orderId = ids.getInt( 1 );
+
+                System.out.println("Debug " + orderId);
+
+            }
+        } catch ( SQLException | ClassNotFoundException ex ) { //temporary error
+            throw new Error( ex.getMessage() );
+        }
+    }
 }
--- src/main/java/maintest/Main.java
+++ src/main/java/maintest/Main.java
@@ -1,5 +1,8 @@
 package maintest;
 
+import dbaccess.OrderMapper;
+import functionlayer.Order;
+
 /**
  *
  * @author malik
@@ -8,6 +11,9 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        
+        OrderMapper om = new OrderMapper();
+        Order o = new Order(4, 10, 10, 2);
+        om.createPreOrder(o);
+        System.out.println("Check: " + o);
     }
 }
