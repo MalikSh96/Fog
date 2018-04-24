73268e00f73e254ea98a1309a5f1bbd86110efef
Malik <malik96sharfo@hotmail.com>
2018-04-24 10:17:18 +0200

    Order table created

--- FogUsers.script.sql
+++ FogUsers.script.sql
@@ -8,17 +8,10 @@ DROP TABLE IF EXISTS `users`;
 
 
 create table `users` (
-<<<<<<< HEAD
 `email` varchar(90),
 `id` int auto_increment primary key,
 `password` varchar(45),
 `role` varchar(20) NOT NULL DEFAULT 'customer',
 UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-=======
-`email` varchar(45),
-`user_id` int auto_increment primary key,
-`password` varchar(45),
-`isAdmin` boolean
-);
->>>>>>> UserDatabase
+
