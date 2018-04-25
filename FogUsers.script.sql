fefa023c0c5ba6053818cb675f45fc4d0154245d
Malik <malik96sharfo@hotmail.com>
2018-04-25 15:47:44 +0200

    j-unit test and table in db, carport

--- FogUsers.script.sql
+++ FogUsers.script.sql
@@ -41,11 +41,3 @@ insert into users(`email`, `password`, `role`) values('maliksharfo@hotmail.com',
 
 -- test test test 
 
-CREATE TABLE `FogUsers`.`carports` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `model` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC),
-  UNIQUE INDEX `model_UNIQUE` (`model` ASC));
