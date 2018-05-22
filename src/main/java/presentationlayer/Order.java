ee7e9717d23ea2ad85350b06c07116c9d4f00d9f
Joklin92 <joklin@live.dk>
2018-05-22 12:29:20 +0200

    send order stuff

--- src/main/java/presentationlayer/Order.java
+++ src/main/java/presentationlayer/Order.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package presentationlayer;
 
 import dbaccess.InventoryMapper;
@@ -23,16 +18,13 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Joklin
- */
 public class Order extends Command {
     
     OrderMapper om = new OrderMapper();
     ItemList itemList = new ItemList();
     InventoryMapper im = new InventoryMapper();
     ItemlistMapper ilm = new ItemlistMapper();
+    int totalPrice = 0;
     
     int userID = 0;
     int length = 0;
@@ -111,7 +103,6 @@ public class Order extends Command {
         heightline = Integer.parseInt(request.getParameter("height"));
         toolshedlength = Integer.parseInt(request.getParameter("toolshedlength"));
         toolshedwidth = Integer.parseInt(request.getParameter("toolshedwidth"));
-        //   PreOrder pre = new PreOrder(userID, length, width, height); 
 
         session.setAttribute("længde", length);
         session.setAttribute("bredde", width);
@@ -134,12 +125,10 @@ public class Order extends Command {
         session.setAttribute("breddemidtentekst", widthtextmiddle);
         session.setAttribute("tagsten", roof_tiles);
         session.setAttribute("højdejord", heightground);
-        Orders ord = new Orders(userID, length, width, height);
-        om.createPreOrder(ord);        
         
-            ilm.addToItemlist(im.getName(11), im.getDescription(11), im.getLength(11), itemList.postAmount(length, width).get(0), om.getOrderId(), im.getId(im.getName(11)));
-            ilm.addToItemlist(im.getName(10), im.getDescription(10), im.getLength(10), itemList.raftAmount(length, width).get(0), om.getOrderId(),im.getId(im.getName(10)));
             ilm.addToItemlist(im.getName(8), im.getDescription(8), im.getLength(8), itemList.remAmount(length).get(0), om.getOrderId(),im.getId(im.getName(8)));
+            ilm.addToItemlist(im.getName(10), im.getDescription(10), im.getLength(10), itemList.raftAmount(length, width).get(0), om.getOrderId(),im.getId(im.getName(10)));
+            ilm.addToItemlist(im.getName(11), im.getDescription(11), im.getLength(11), itemList.postAmount(length, width).get(0), om.getOrderId(), im.getId(im.getName(11)));
             ilm.addToItemlist(im.getName(15), im.getDescription(15), im.getLength(15), itemList.roofAmount(length, width).get(0), om.getOrderId(),im.getId(im.getName(15)));
             ilm.addToItemlist(im.getName(17), im.getDescription(17), 0, roofScrewAmount, om.getOrderId(),im.getId(im.getName(17)));
             ilm.addToItemlist(im.getName(19), im.getDescription(19), 0, universalRightAmount, om.getOrderId(),im.getId(im.getName(19)));
@@ -147,54 +136,20 @@ public class Order extends Command {
             ilm.addToItemlist(im.getName(22), im.getDescription(22), 0, bracketScrewAmount, om.getOrderId(),im.getId(im.getName(22)));
             ilm.addToItemlist(im.getName(23), im.getDescription(23), 0, carriageBoltAmount, om.getOrderId(),im.getId(im.getName(23)));
             ilm.addToItemlist(im.getName(24), im.getDescription(24), 0, squareSlicesAmount, om.getOrderId(),im.getId(im.getName(24)));
-//            im.updateStatus(11, itemList.postAmount(length, width).get(0));
-//            im.updateStatus(10, itemList.raftAmount(length, width).get(0));
-//            im.updateStatus(8, itemList.remAmount(length).get(0));
-//            im.updateStatus(15, itemList.roofAmount(length, width).get(0));
-//            im.updateStatus(17, roofScrewAmount);
-//            im.updateStatus(19, universalRightAmount);
-//            im.updateStatus(20, universalLeftAmount);
-//            im.updateStatus(22, bracketScrewAmount);
-//            im.updateStatus(23, carriageBoltAmount);
-//            im.updateStatus(24, squareSlicesAmount);
-            
-            
-            List<String> content = ilm.getFullItemlist(om.getOrderId());
 
-            
-            //filewriter virker når programmet køres lokalt. virker ikke via DO Droplet
-        try {           
-
-          // File file = new File("C:/Users/Jokli/Documents/repos/Fog/Styklister/order nr " + om.getOrderId() + " - Stykliste.txt");
-           File path = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator +"Styklister"); 
-           File file = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator +"Styklister" + File.separator + "order nr " + om.getOrderId() + " - Stykliste.txt");
-           
-           if(!path.exists()) {
-               try {
-                   path.mkdir();
-               } catch (Exception e) {
-                   e.printStackTrace();
-               }
-           }
-           if(!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            }
-           
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-            
-            for (int i = 0; i < content.size(); i++) {
-                bw.write(content.get(i));
-                bw.newLine();
-            }            
-            bw.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+            totalPrice += im.getPrice(8);
+            totalPrice += im.getPrice(10);
+            totalPrice += im.getPrice(11);
+            totalPrice += im.getPrice(15);
+            totalPrice += im.getPrice(17);
+            totalPrice += im.getPrice(19);
+            totalPrice += im.getPrice(20);
+            totalPrice += im.getPrice(22);
+            totalPrice += im.getPrice(23);
+            totalPrice += im.getPrice(24);
          
+        Orders ord = new Orders(userID, length, width, height, totalPrice, true, "priced");
+        om.createPreOrder(ord);        
         return "order";
     }    
 }
