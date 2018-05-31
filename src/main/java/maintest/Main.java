8641659170642bad6356b923d2d5a414a935645b
Dennis <joklin@live.dk>
2018-05-31 06:41:06 +0200

    maintest

--- src/main/java/maintest/Main.java
+++ src/main/java/maintest/Main.java
@@ -6,6 +6,8 @@ import businesslayer.ItemList;
 import businesslayer.UniversalExceptions;
 import businesslayer.User;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -14,17 +16,28 @@ public class Main {
         Constants con = new Constants();
         BusinessFacade bf = con.getBf();
         User us = bf.getUser(5);
-        int id = 0;
-        if (us.isAdmin(bf.getUserRole(us.getId())) && !bf.getOrder(id).isOrderConfirmed() || bf.getUserRole(bf.getUserId(us.getEmail())).equals("storagechief") && !bf.getOrder(id).isOrderConfirmed()) {
-            
-        }
-        
-        if(bf.getUserRole(bf.getUserId(us.getEmail())).equals("seller")){ System.out.println(bf.getUserRole(7));}
-        System.out.println(bf.getUserRole(5));
-        ItemList il = new ItemList();
-        System.out.println("Post: " + il.postAmount(800, 600));
-        System.out.println("Raft: " + il.raftAmount(800, 400));
-        System.out.println("Rem: " + il.remAmount(800));
+//        int id = 0;
+//        if (us.isAdmin(bf.getUserRole(us.getId())) && !bf.getOrder(id).isOrderConfirmed() || bf.getUserRole(bf.getUserId(us.getEmail())).equals("storagechief") && !bf.getOrder(id).isOrderConfirmed()) {
+//            
+//        }
+        bf.sendOrder(4);
+        boolean possible = true;
+        List<Integer> itemIds = bf.getFullItemlistId(8);
+        System.out.println(bf.getFullItemlist(8));
+//        List<Integer> wrongIds = new ArrayList<>();
+//       for (int i = 0; i < itemIds.size(); i++) {
+//            if (!bf.updateStatus(itemIds.get(i), bf.getAmount(8, itemIds.get(i)))) {
+//                possible = false;
+//                wrongIds.add(itemIds.get(i));
+//            }
+//            System.out.println(possible);
+
+//        if(bf.getUserRole(bf.getUserId(us.getEmail())).equals("seller")){ System.out.println(bf.getUserRole(7));}
+//        System.out.println(bf.getUserRole(5));
+//        ItemList il = new ItemList();
+//        System.out.println("Post: " + il.postAmount(800, 600));
+//        System.out.println("Raft: " + il.raftAmount(800, 400));
+//        System.out.println("Rem: " + il.remAmount(800));
 
     }
 }
