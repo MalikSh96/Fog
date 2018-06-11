8489f8af024cf750798db1381468ef26fcc2cb1e
UnknownQuantity <jacob_wr@live.dk>
2018-06-11 11:01:30 +0200

    SpecificOrder Cleanup

--- src/main/java/presentationlayer/SpecificOrder.java
+++ src/main/java/presentationlayer/SpecificOrder.java
@@ -12,7 +12,6 @@ public class SpecificOrder extends Command {
 
     int chosenId = 0;
     Constants con = new Constants();
-    BusinessFacade bf = con.getBf();
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws UniversalExceptions {
@@ -21,15 +20,6 @@ public class SpecificOrder extends Command {
 
         if (request.getParameter("chosenid").length() > 0) {
             chosenId = Integer.parseInt(request.getParameter("chosenid"));
-        }
-        if (curUs.isAdmin(bf.getUserRole(curUs.getId())) && !bf.OrderIdExists(chosenId)) {
-            return "adminpage";
-        } else if (bf.getUserRole(bf.getUserId(curUs.getEmail())).equals("storageworker") && !bf.OrderIdExists(chosenId)) {
-            return "storageworkerpage";
-        } else if (bf.getUserRole(bf.getUserId(curUs.getEmail())).equals("storagechief") && !bf.OrderIdExists(chosenId)) {
-            return "storagechiefpage";
-        } else if (!curUs.isAdmin(bf.getUserRole(curUs.getId())) && curUs.getId() != bf.getUserId(chosenId) && bf.getUserRole(bf.getUserId(curUs.getEmail())).equals("storageworker") && bf.getUserRole(bf.getUserId(curUs.getEmail())).equals("storagechief")) {
-            return "myorders";
         }
         session.setAttribute("orderid", chosenId);
         return "specificOrder";
